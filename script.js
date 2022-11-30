0020a87350d51fcaaa112f7132943d51ed64ea4f
Tyom-Sargsyan <artyomsargsyan68@gmail.com>
2022-11-30 16:01:38 +0400

    massive changes

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
+const texts = [
+  "Մերսի քեզ, մեր ծանոթության առաջին օրվանից իմ կյանքը ավելի սիրուն, պուպուշ, հիասքանչ, կախարդական դարձնելու համար",
+  "Երևի իրոք նշան էր, որ հենց դու ուղարկեցիր Led Zeppelin ի երգի վիդեոն",
+  "Եթե դուք հասել եք այս սրտիկին, ուրեմն հերիքա սպասեք հրաշքի, այն արդեն տեղի է ունեցել, զանգեք Ծյոմին",
+  `Չեմ ուզում ինձ դնեմ իմ երազած տղու տեղը, բայց պիտի ասեմ. "I feel different when I'm with you"`,
+  "Պատրաստ եմ անվերջ լսել, թե ոնց ես 1 վայրկյան ժամանակահատվածում հարյուրավոր մտքեր արտահայտում",
+  "Once Ստառուշկա <br> Always Ստառուշկա",
+  "J'ai fait ce site, pour exprimer mes sentiments, que j'ai pour vous.  Il y a tant à dire, mais ici je n'ai séparé que les choses les plus importantes.",
+  "Язык может скрывать истину, а глаза – никогда!",
+  "Want to give you the same butterflies, as you gave to me",
+  '<img src="frame.png" width="200" height="300" alt="" />',
+];
 let arr = [
-  `Բարև Ձեզ, պատրաստ եք՞`,
+  `Պատրաստ եք՞`,
   `Քանի տարեկան եք՞`,
   `Բանակում Ծառայել եք՞`,
-  `Աշխատում եք թե ոչ՞`,
-  `Ամուսնաացած եք թե ոչ՞`,
+  `Աշխատում եք՞`,
+  `Ամուսնացած եք՞`,
   `Կուզեք ընկեր ունենալ՞`,
   `Գիտեիք՞ որ Կիթառ նվագող զույգերը ավելի երջանիկ են միմյանց հետ`,
   `Գիտեիք՞ որ դուք շատ գեղեցիկ եք`,
-  `Եթե Ծյոմը ձեզ հրավիրեր հանդիպման ինչ՞ կպատասխանեիք`,
+  `Եթե Ծյոմը ձեզ հրավիրեր դեյթի ինչ՞ կպատասխանեիք`,
 ];
+
+texts.forEach((text) => {
+  let div = document.createElement("div");
+  div.classList.add("snowflake");
+  let p = document.createElement("p");
+  p.innerHTML = text;
+  div.innerHTML += `<img src="hearts/heart${Math.round(
+    Math.random() * 5 + 1
+  )}.svg" alt="heart"/>`;
+  div.append(p);
+
+  document.querySelector(".snowflakes").prepend(div);
+});
+
 let i = 0;
-console.log(arr[5]);
 let a = document.getElementById(`question`);
 let yesButton = document
   .querySelectorAll(`button`)
@@ -28,18 +53,16 @@ const getPercent = () => {
 };
 
 function initQuestion() {
+  4;
   if (i === arr.length - 1) {
-    console.log("last questions");
     document.querySelector("#no").style.transition = "0s";
     document.querySelector("#no").style.pointerEvents = "none";
     document.getElementById(`noParent`).onmouseover = () => {
-
       let topPos = getPercent();
       let leftPos = getPercent();
       if ((topPos === leftPos) === "0px") {
         leftPos = !Math.round(Math.random()) ? "-130%" : "130%";
       }
-
       document.querySelector("#no").style.top = topPos;
       document.querySelector("#no").style.left = leftPos;
     };
@@ -52,27 +75,23 @@ function initQuestion() {
   if (i === arr.length) {
     document.querySelector(".section").style.display = "none";
     document.getElementById("background").style.filter = "none";
-    document.querySelector("#music").play();
-    // document.querySelector(`.snowflakes`).setAttribute(`id`, `new`)
-    let fil = document.querySelectorAll(`.snowflake`)
-    let initialHeart =  document.querySelector(`.snowflake1 > svg`)
-    let fil1 = document.querySelector(`.myVideo`)
-    initialHeart.style.width = `700px`
-    initialHeart.style.height = `auto`
-    // initialHeart.style.top = `-200px`
-    initialHeart.style.transition = `2s`
-    fil1.style.filter = `blur(4rem)`
-    fil.forEach(el => {
-      el.style.filter = `blur(4rem)`
-    })
-    initialHeart.addEventListener(`click`, closeIt)
+    // document.querySelector("#music").play();
+    let fil = document.querySelectorAll(`.snowflake`);
+    let initialHeart = document.querySelector(`.snowflake1 > img`);
+    let videoFilter = document.querySelector(`.myVideo`);
+    initialHeart.classList.add(`initial`)
+    videoFilter.style.filter = `blur(4rem)`;
+    fil.forEach((el) => {
+      el.style.filter = `blur(4rem)`;
+    });
+    initialHeart.addEventListener(`click`, closeIt);
     function closeIt() {
-      let flex = document.querySelector(`.flex`)
-      flex.style.display = `none`
-      fil1.style.filter = `none`
-      fil.forEach(el => {
-        el.style.filter = `none`
-      })
+      let flex = document.querySelector(`.flex`);
+      flex.style.display = `none`;
+      videoFilter.style.filter = `none`;
+      fil.forEach((el) => {
+        el.style.filter = `none`;
+      });
     }
   } else {
     a.innerHTML = arr[i];
@@ -81,49 +100,40 @@ function initQuestion() {
 }
 initQuestion();
 
-
 //--------------------------------------------------------------------------
-
-// let mainElem = document.getElementById(`new`);
-// mainElem.addEventListener(`click`, cancelAnimation);
-// function cancelAnimation() {
-//   mainElem.style.display = `none`;
-//   hearts.forEach((el) => {
-//     el.style.filter = `none`;
-//   });
-// }
 let hearts = document.querySelectorAll(`.snowflake`);
 let text = document.querySelectorAll(`p`);
 
 hearts.forEach((snowFlake) => {
   snowFlake.addEventListener(`click`, () => {
-    snowFlake.classList.toggle("dontMove");
-    if (snowFlake.querySelector("p").classList.contains("openText")) {
-      snowFlake.querySelector("p").classList.remove("openText");
-    } else {
-      setTimeout(() => {
-        snowFlake.querySelector("p").classList.add("openText");
-      }, 1000);
+    if (!snowFlake.classList.contains("dontMove")) {
+      snowFlake.setAttribute("leftPos", snowFlake.offsetLeft);
     }
+    snowFlake.classList.toggle("dontMove");
   });
 });
 
 const jsAnimation = (snowFlake, dirTog) => {
   if (snowFlake.classList.contains("dontMove")) {
     return;
-  }
+  } 
   const hor = dirTog ? 200 : -200;
+  let initialLeftPos = snowFlake.offsetLeft;
+  if (snowFlake.getAttribute("leftPos")) {
+    initialLeftPos = snowFlake.getAttribute("leftPos");
+    snowFlake.removeAttribute("leftPos");
+  }
+  snowFlake.style.left = +initialLeftPos + hor + "px";
 
-  snowFlake.style.left = snowFlake.offsetLeft + hor + "px";
   if (snowFlake.offsetTop >= window.innerHeight) {
     snowFlake.style.transitionDuration = "0s";
     snowFlake.style.top = 0 + "px";
     setTimeout(() => {
-      snowFlake.style.transitionDuration = "2.5s";
+      snowFlake.style.transitionDuration = "1.8s";
       snowFlake.style.top = snowFlake.offsetTop + 250 + "px";
     }, 0);
   } else {
-    snowFlake.style.transitionDuration = "2.5s";
+    snowFlake.style.transitionDuration = "1.8s";
     snowFlake.style.top = snowFlake.offsetTop + 250 + "px";
   }
 };
@@ -141,50 +151,9 @@ const animateTheSelection = ({ delayTime = 0, selector }) => {
   }, delayTime);
 };
 
-animateTheSelection({
-  delayTime: 2000,
-  selector: `.snowflake:nth-child(1)`,
-});
-animateTheSelection({
-  selector: `.snowflake:nth-child(2)`,
-});
-animateTheSelection({
-  delayTime: 2700,
-  selector: `.snowflake:nth-child(3)`,
-});
-animateTheSelection({
-  delayTime: 1500,
-  selector: `.snowflake:nth-child(4)`,
-});
-animateTheSelection({
-  delayTime: 2500,
-  selector: `.snowflake:nth-child(5)`,
-});
-animateTheSelection({
-  delayTime: 3500,
-  selector: `.snowflake:nth-child(2)`,
-});
-animateTheSelection({
-  delayTime: 500,
-  selector: `.snowflake:nth-child(5)`,
-});
-animateTheSelection({
-  delayTime: 1300,
-  selector: `.snowflake:nth-child(6)`,
-});
-animateTheSelection({
-  delayTime: 700,
-  selector: `.snowflake:nth-child(7)`,
-});
-animateTheSelection({
-  delayTime: 2200,
-  selector: `.snowflake:nth-child(8)`,
-});
-animateTheSelection({
-  delayTime: 3000,
-  selector: `.snowflake:nth-child(9)`,
-});
-animateTheSelection({
-  delayTime: 1000,
-  selector: `.snowflake:nth-child(10)`,
-});
+for (let i = 1; i <= 10; i++) {
+  animateTheSelection({
+    delayTime: Math.round(Math.random() * 5000),
+    selector: `.snowflake:nth-child(${i})`,
+  });
+}
